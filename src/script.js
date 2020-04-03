609c65a93f03954f3f23dd1f337c428bf7ed90f3
Denis <suboch.denny@gmail.com>
2020-04-03 14:17:37 +0300

    fix: fix eslint errors

--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
 const body = document.querySelector('body');
 
-let isEng = true;
+let lang = localStorage.getItem('lang') ? localStorage.getItem('lang') : 'eng';
 let isShiftClicked = false;
 let isCapsLockClicked = false;
 
 
+const changeLangState = (language) => localStorage.setItem('lang', language);
+
 const keyCodes = [
   'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
   'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
-  'ShiftLeft', 'Backslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+  'ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
   'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'MetaRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 const engKeys = [
   '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
@@ -21,14 +23,14 @@ const engCapsKeys = [
   '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 'Del',
   'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter',
-  'Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift',
+  'Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift ',
   'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Win', 'Ctrl', '←', '↓', '→',
 ];
 const engShiftKeys = [
   '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace',
   'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 'Del',
   'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter',
-  'Shift', '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift',
+  'Shift', '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift ',
   'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Win', 'Ctrl', '←', '↓', '→',
 ];
 
@@ -37,21 +39,21 @@ const ruKeys = [
   'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'Del',
   'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
-  'Shift', '\\', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift',
+  'Shift', '\\', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift ',
   'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Win', 'Ctrl', '←', '↓', '→',
 ];
 const ruCapsKeys = [
   'Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'Del',
   'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter',
-  'Shift', '\\', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '↑', 'Shift',
+  'Shift', '\\', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '↑', 'Shift ',
   'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Win', 'Ctrl', '←', '↓', '→',
 ];
 const ruShiftKeys = [
   'Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace',
   'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'Del',
   'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter',
-  'Shift', '/', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'Shift',
+  'Shift', '/', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'Shift ',
   'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Win', 'Ctrl', '←', '↓', '→',
 ];
 
@@ -70,7 +72,6 @@ body.append(inputField);
 const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 body.append(keyboard);
-
 
 function addKeys(keys) {
   keys.forEach((keyValue, index) => {
@@ -101,8 +102,6 @@ document.addEventListener('keydown', (event) => {
       elem.classList.add('key--active');
       if (!specialKeys.includes(event.code)) {
         inputField.setRangeText(elem.textContent, inputField.selectionStart, inputField.selectionEnd, 'end');
-        // inputTextContent = elem.textContent;
-        // inputField.value = inputTextContent;
       }
     }
   });
@@ -111,25 +110,16 @@ document.addEventListener('keydown', (event) => {
       if (event.repeat) return;
       isCapsLockClicked = !isCapsLockClicked;
       if (isCapsLockClicked) {
-        keys.forEach((elem) => {
-          if (event.code === elem.getAttribute('code')) {
-            elem.classList.add('key--active');
-          }
-        });
+        keyboard.querySelector("div[code='CapsLock']").classList.add('key--active');
 
         if (isShiftClicked) {
-          isEng ? changeKeys(engCapsKeys) : changeKeys(ruCapsKeys);
+          lang == 'eng' ? changeKeys(engShiftKeys) : changeKeys(ruShiftKeys); // ??????????????????????????????????
         } else {
-          isEng ? changeKeys(engCapsKeys) : changeKeys(ruCapsKeys);
+          lang == 'eng' ? changeKeys(engCapsKeys) : changeKeys(ruCapsKeys);
         }
       } else {
-        keys.forEach((elem) => {
-          if (event.code === elem.getAttribute('code')) {
-            elem.classList.remove('key--active');
-          }
-        });
-
-        isEng ? changeKeys(engKeys) : changeKeys(ruKeys);
+        keyboard.querySelector("div[code='CapsLock']").classList.remove('key--active');
+        changeKeys(lang === 'eng' ? engKeys : ruKeys);
       }
       break;
     case 'ShiftLeft':
@@ -137,9 +127,9 @@ document.addEventListener('keydown', (event) => {
       isShiftClicked = !isShiftClicked;
       if (isShiftClicked) {
         if (isCapsLockClicked) {
-          isEng ? changeKeys(engKeys) : changeKeys(ruKeys);
+          changeKeys(lang === 'eng' ? engKeys : ruKeys);
         } else {
-          isEng ? changeKeys(engShiftKeys) : changeKeys(ruShiftKeys);
+          changeKeys(lang === 'eng' ? engShiftKeys : ruShiftKeys);
         }
       }
       break;
@@ -157,12 +147,12 @@ document.addEventListener('keydown', (event) => {
   }
 
   if (event.ctrlKey && event.altKey) {
-    isEng = !isEng;
-
+    lang = lang === 'eng' ? 'ru' : 'eng';
+    changeLangState(lang);
     if (isCapsLockClicked) {
-      isEng ? changeKeys(engCapsKeys) : changeKeys(ruCapsKeys);
+      changeKeys(lang === 'eng' ? engCapsKeys : ruCapsKeys);
     } else {
-      isEng ? changeKeys(engKeys) : changeKeys(ruKeys);
+      changeKeys(lang === 'eng' ? engKeys : ruKeys);
     }
   }
 });
@@ -182,9 +172,9 @@ document.addEventListener('keyup', (event) => {
       isShiftClicked = !isShiftClicked;
       if (!isShiftClicked) {
         if (isCapsLockClicked) {
-          isEng ? changeKeys(engCapsKeys) : changeKeys(ruCapsKeys);
+          changeKeys(lang === 'eng' ? engCapsKeys : ruCapsKeys);
         } else {
-          isEng ? changeKeys(engKeys) : changeKeys(ruKeys);
+          changeKeys(lang === 'eng' ? engKeys : ruKeys);
         }
       }
       break;
@@ -195,10 +185,4 @@ document.addEventListener('keyup', (event) => {
 });
 
 
-addKeys(engKeys);
-
-
-const textarea = document.querySelector('textarea');
-textarea.addEventListener('click', (event) => {
-  event.preventDefault();
-});
+addKeys(lang === 'eng' ? engKeys : ruKeys);
